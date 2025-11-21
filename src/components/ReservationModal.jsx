db49157f33103a3df5f78c578824762037a851f8
syngha.han <gkstmdgk2731@naver.com>
2025-11-22 00:08:24 +0900

    Fix: Improve modal scrolling on mobile

--- src/components/ReservationModal.jsx
+++ src/components/ReservationModal.jsx
@@ -19,7 +19,7 @@ export default function ReservationModal({ event, onClose }) {
                 </div>
 
                 {/* Content */}
-                <div className="overflow-y-auto p-4 flex flex-col gap-4 bg-gray-100 dark:bg-gray-900">
+                <div className="flex-1 min-h-0 overflow-y-auto overscroll-contain p-4 flex flex-col gap-4 bg-gray-100 dark:bg-gray-900">
                     <div className="bg-white dark:bg-gray-800 p-2 rounded-lg shadow-sm">
                         <img
                             src={getImagePath(event.image)}
